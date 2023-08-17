b4a20a9e1ca0c6d62ab109569496c7eda16ecd39
Ruslan Symonenko <symonenkors@gmail.com>
2023-08-17 17:12:26 +0300

    made router for all pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,13 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer } from 'react-toastify';
 import { getMe, checkIsAuth } from "./store/slices/authSlice";
+import { routes } from './router/routes';
 
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import Layout from "./layout/Layout";
-
-const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
-const MyPostsPage = lazy(() => import('./pages/MyPostsPage/MyPostsPage'));
-const PostPage = lazy(() => import('./pages/PostPage/PostPage'));
-const NewPostPage = lazy(() => import('./pages/NewPostPage/NewPostPage'));
-const RegisterPage = lazy(() => import('./pages/RegisterPage/RegisterPage'));
-const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
 
 
 function App() {
@@ -34,20 +28,20 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout/>}>
-          <Route index path="/" element={<HomePage/>}/>
-          <Route path="/register" element={<RegisterPage/>}/>
-          <Route path="/login" element={<LoginPage/>}/>
-          <Route path="/myposts" element={<MyPostsPage/>}/>
-
-          <Route path="/post/:id" element={<PostPage/>}/>
-
-          <Route 
-            path="/newpost" 
-            element={
-              <PrivateRoute>
-                <NewPostPage/>
-              </PrivateRoute>
-            }/>
+          {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              element={
+                route.private ? (
+                  <PrivateRoute>{route.element}</PrivateRoute>
+                ) : (
+                  route.element
+                )
+              }
+            />
+            ))
+          }
         </Route>
       </Routes>
 
---
+++ client/src/router/routes.js
@@ -0,0 +1,41 @@
+import { lazy } from "react";
+
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const MyPostsPage = lazy(() => import('../pages/MyPostsPage/MyPostsPage'));
+const PostPage = lazy(() => import('../pages/PostPage/PostPage'));
+const NewPostPage = lazy(() => import('../pages/NewPostPage/NewPostPage'));
+const RegisterPage = lazy(() => import('../pages/RegisterPage/RegisterPage'));
+const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
+
+export const routes = [
+  {
+    path: '/',
+    element: <HomePage/>,
+    private: false,
+  },
+  {
+    path: '/register',
+    element: <RegisterPage/>,
+    private: false,
+  },
+  {
+    path: '/login',
+    element: <LoginPage/>,
+    private: false,
+  },
+  {
+    path: '/myposts',
+    element: <MyPostsPage/>,
+    private: true,
+  },
+  {
+    path: '/post/:id',
+    element: <PostPage/>,
+    private: false,
+  },
+  {
+    path: '/newpost',
+    element: <NewPostPage/>,
+    private: true,
+  },
+];
